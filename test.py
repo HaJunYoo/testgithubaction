767b6f58745f76265a58eeee2cf5c2a85f18b07a
Ha Jun Yoo <58240677+HaJunYoo@users.noreply.github.com>
2023-02-14 15:16:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 import datetime
+import requests
+from bs4 import BeautifulSoup
 
-print(f"hello world! It's {datetime.datetime.now()}")
 
+print(f"hello! It's {datetime.datetime.now()}. Start Crawling")
+
+url = 'https://ridibooks.com/category/new-releases/2200'
+response = requests.get(url)
+response.encoding = 'utf-8'
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+
+bookservices = soup.select('.title_text')
+for no, book in enumerate(bookservices, 1):
+    print(no, book.text.strip())
